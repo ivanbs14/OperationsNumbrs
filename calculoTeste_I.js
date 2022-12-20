26347b09e8c2882574d1395dc23d06a49ad7cdd7
Ivan Barbosa <ivanbarbosag@gmail.com>
2022-12-20 11:50:20 -0300

    ajust codigo

--- calculoTeste_I.js
+++ calculoTeste_I.js
@@ -3,9 +3,15 @@ let numb1 = Number(prompt("digite o primeiro numero:"))
 let numb2 = Number(prompt("digite o primeiro segundo:"))
 
 let sum = numb1 + numb2
+let sub = numb1 - numb2
 let multip = numb1 * numb2
 let divis = numb1 / numb2
 let rest = numb1 % numb2
+
+alert(`A resultado da subtração é ${sub}.`)
+alert(`A resultado da multiplicação é ${multip}.`)
+alert(`A resultado da divisão é ${divis.toFixed(2)}.`)
+alert(`O resto da divisão é ${rest}.`)
 
 if(sum % 2 == 0) {
     alert(`A resultado da soma é ${sum} este numero é PAR.`)
@@ -16,5 +22,5 @@ if(sum % 2 == 0) {
 if(numb1 == numb2) {
     alert(`O primeiro numero é igual ao segundo numero`)
 } else {
-    alert(`O primeiro numero é diferente do segundo numero`)
+    alert(`O primeiro numero que é ${numb1}, é diferente do segundo que é numero ${numb2}`)
 }
